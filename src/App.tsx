1e9a32d787f058f1184b62644c6d32b2c480ce51
S <siddtheone@gmail.com>
2025-11-14 11:59:17 +0530

    Clean up and basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { DataGrid } from "@mui/x-data-grid";
+import type { GridColDef } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+
+const columns: GridColDef[] = [
+  { field: "tradeId", headerName: "Trade Id" },
+  { field: "version", headerName: "Version" },
+  { field: "counterPartyId", headerName: "Counter-Party Id" },
+  { field: "bookId", headerName: "Book-Id" },
+  { field: "maturityDate", headerName: "Maturity Date" },
+  { field: "createdDate", headerName: "Created Date" },
+  { field: "expired", headerName: "Expired" },
+];
+
+const today = new Date().toLocaleDateString("en-GB");
+
+const rows = [
+  {
+    id: 1,
+    tradeId: "T1",
+    version: 1,
+    counterPartyId: "CP-1",
+    bookId: "B1",
+    maturityDate: "20/05/2020",
+    createdDate: today,
+    expired: "N",
+  },
+  {
+    id: 2,
+    tradeId: "T2",
+    version: 2,
+    counterPartyId: "CP-2",
+    bookId: "B1",
+    maturityDate: "20/05/2021",
+    createdDate: today,
+    expired: "N",
+  },
+  {
+    id: 3,
+    tradeId: "T2",
+    version: 1,
+    counterPartyId: "CP-1",
+    bookId: "B1",
+    maturityDate: "20/05/2021",
+    createdDate: "14/03/2015",
+    expired: "N",
+  },
+  {
+    id: 4,
+    tradeId: "T3",
+    version: 3,
+    counterPartyId: "CP-3",
+    bookId: "B2",
+    maturityDate: "20/05/2014",
+    createdDate: today,
+    expired: "Y",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: { paginationModel: { pageSize: 5 } },
+        }}
+        pageSizeOptions={[5, 10, 20]}
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import ComponentProvider from './ComponentProvider';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import ComponentProvider from "./ComponentProvider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ComponentProvider>
       <App />
     </ComponentProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
